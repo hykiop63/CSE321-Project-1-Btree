166c29739ee3431f1e0563491d283aee50092499
hykiop <hykiop@gamil.com>
2026-04-24 13:54:57 +0900

    implement search of B

--- B.cpp
+++ B.cpp
@@ -2,7 +2,9 @@
 #include "B.h"
 #include <iostream>
 
-Node::Node(int order){
+Node::Node(int order,Node* p){
+    parent=p;
+    is_leaf=false;
     keys.reserve(order);
     rids.reserve(order);
     child_ptrs.reserve(order+1);
@@ -17,16 +19,41 @@ void B::split(int key){
 void B::merge(int key){
     std::cout<<key<<"\n";
 }
+int B::inNode_find(int key,Node* curr_n){
+    int left=0;
+    int right=curr_n->keys.size()-1;
+    int mid=(right+left)/2;
+    while(left<=right){
+        if(key==curr_n->keys[mid]) return mid;//노드내에 존재
+        else if(key<curr_n->keys[mid]) right=mid-1;
+        else left=mid+1;
+        mid=(right+left)/2;
+    }
+    return left;//자식으로 이동해야함
+}
+// B 클래스 내부의 search 함수 구현
+std::pair<Node*,int> B::inter_search(int key) {
+    std::cout << key << " 검색 로직 실행\n";
+    Node* now_check=root;
+    while(now_check!=nullptr){
+        int idx=inNode_find(key,now_check);
+        if(idx<now_check->keys.size() && now_check->keys[idx]==key)
+            return {now_check,idx};
+        if(now_check->is_leaf) break;
+        now_check = now_check->child_ptrs[idx];
+    }
+}
 // B 클래스 내부의 insert 함수 구현
-void B::insert(int key) {
+void B::insert(int key,int rid) {
     std::cout << key << " 삽입 로직 실행\n";
     // 실제 B-Tree 노드 분할 및 삽입 로직 작성
+    
 }
 void B::remove(int key){
     std::cout << key << " 검색 로직 실행\n";
 }
-// B 클래스 내부의 search 함수 구현
-void B::search(int key) {
-    std::cout << key << " 검색 로직 실행\n";
-    // 실제 검색 로직 작성
+int B::search(int key){
+    std::pair<Node*,int> result=inter_search(key);
+    return result.first==nullptr ? -1 : 
+            result.first->rids[result.second];
 }
--- B.h
+++ B.h
@@ -1,10 +1,13 @@
 #pragma once
 #include<vector>
+#include<utility>
 struct Node {
+    Node* parent;
+    bool is_leaf;
     std::vector<int> keys;
     std::vector<int> rids;
     std::vector<Node*> child_ptrs;
-    Node(int order);
+    Node(int order,Node* p);
 };
 class B{
     private:
@@ -12,11 +15,14 @@ class B{
         Node* root;
         void split(int key);
         void merge(int key);
+        int inNode_find(int key,Node* curr_n);
+        std::pair<Node*,int> inter_search(int key);
     public:
         B(int d);
-        void insert(int key);
+        void insert(int key,int rid);
         void remove(int key);
-        void search(int key);
+        int search(int key);
+        
 
 
 };
